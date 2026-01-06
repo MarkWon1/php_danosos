7f547a812a6b8086ce0ce1ae00fb733d580fdc03
unknown <ermiedanosos@gmail.com>
2026-01-06 08:54:49 +0800

    bago

---
+++ pages/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <h1>LOGIN</h1>
+    <form action="">
+        <input type="text" id="username" class="username" name="username" placeholder="username">
+        <input type="password" id="password" class="password" name="password" placeholder="password">
+        <button type="submit">Submit</button>
+    </form>
+   
+</body>
+</html>
