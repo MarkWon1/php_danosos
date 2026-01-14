f67645a7c3fff1b739a934b62355c0883e6a14a3
unknown <ermiedanosos@gmail.com>
2026-01-14 08:55:24 +0800

    added table and list

--- pages/retrievePage.php
+++ pages/retrievePage.php
@@ -1,3 +1,12 @@
+<?php
+$list =['Leo', 'Sangwon', 'Arno', 'Anxin', 'Heli', 'Solon', 'Shion', 'Niki', 'Markwon', 'Dex', 'Shade'];
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <?php
@@ -11,5 +20,47 @@ include "resources/navigation.php";
 ?>
 
 <h1>This Is Retrieve Page!!!</h1>
+<ul>
+<?php
+      foreach($list as $item){
+
+?>
+
+
+      <li> <?= $item?> </li>
+
+
+<?php
+      }
+?>
+</ul>
+
+      <table>
+            <tr>
+                  <th>Name</th>
+                  <th>Action</th>
+            </tr>
+
+      
+     
+<?php
+      foreach($list as $item){
+
+?>
+
+
+      <tr>
+            <td><?=  $item ?></td>
+            <td><button>View</button></td>
+      </tr>
+
+
+<?php
+      }
+?>
+
+
+
+      </table>
 </body>
 </html>
